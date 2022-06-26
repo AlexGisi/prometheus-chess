447dd29100dc436624a9da662846a2210c165e74
Alex Gisi <apgisi11@gmail.com>
2022-06-26 21:10:08 +0200

    Improve PVTable performance

--- src/PVTable.cpp
+++ src/PVTable.cpp
@@ -12,8 +12,6 @@
 PVTable::PVTable(int size) {
     entries = ( size / (int) sizeof(PVEntry) ) - 2;  // Subtract two to avoid any out-of-bounds.
     table = std::make_unique<PVEntry[]>(size / (int) sizeof(PVEntry));
-
-    clear();
 }
 
 void PVTable::clear() {
@@ -24,24 +22,11 @@ void PVTable::clear() {
 PVTable::PVTable(const PVTable &rhs) {
     entries = rhs.entries;
     table = std::make_unique<PVEntry[]>(rhs.entries);
-    clear();
-
-    for(int i=0; i < entries; i++) {
-        table[i] = rhs.table[i];
-    }
 }
 
 PVTable& PVTable::operator=(const PVTable &rhs) {
-    // This self-assignment check is considered bad design.
-    if(&rhs == this)
-        return *this;
-
     entries = rhs.entries;
     table = std::make_unique<PVEntry[]>(rhs.entries);
-
-    for(int i=0; i < entries; i++) {
-        table[i] = rhs.table[i];
-    }
 
     return *this;
 }
@@ -53,6 +38,7 @@ void PVTable::store(const PosKey& key, const Move& mv) {
     int idx = (int) (key.val & INT_MAX) % entries;
     assert(idx >= 0 && idx <= entries-1);
 
+    table[idx] = PVEntry();
     table[idx].move = mv;
     table[idx].posKey = key;
 }
